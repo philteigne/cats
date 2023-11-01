e7e11dfc4e8de4049a98300f600e001cabe5ad72
Phil <philippe.teigne@gmail.com>
2023-11-01 17:47:07 -0400

    add function to asyncBreeds.js

---
+++ asyncBreeds.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) callback(data);
+  });
+};
+
+const bombay = breedDetailsFromFile('Bombay', a => console.log(`Return Value: ${a}`));
